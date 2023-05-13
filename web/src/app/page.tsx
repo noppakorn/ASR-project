b71fb3d0f5b8351387483cadcd55b4fd17f33264
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2023-05-13 23:45:43 +0700

    web: audio record, playback and post req

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import axios from "axios";
+import Link from "next/link";
 import { useRef, useState } from "react";
 
 const mimeType = "audio/webm";
@@ -9,16 +11,21 @@ export enum RecordingStatus {
   Paused,
 }
 
+export type OrderResponse = {
+  text: string;
+};
+const API_URL = "http://localhost:8000";
+
 export default function Home() {
   const [permission, setPermission] = useState<boolean>(false);
 
-  const mediaRecorder = useRef(null);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
   const [recordingStatus, setRecordingStatus] = useState<RecordingStatus>(
     RecordingStatus.Inactive
   );
   const [stream, setStream] = useState<MediaStream | null>(null);
-  const [audioChunks, setAudioChunks] = useState<any[]>([]);
-  const [audio, setAudio] = useState<any | null>(null);
+  const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
+  const [audio, setAudio] = useState<string | null>(null);
 
   const getMicrophonePermission = async () => {
     if ("MediaRecorder" in window) {
@@ -41,27 +48,110 @@ export default function Home() {
 
   const startRecording = async () => {
     if (stream) {
-      const media = new MediaRecorder(stream);
+      setRecordingStatus(RecordingStatus.Recording);
+      const media = new MediaRecorder(stream, { mimeType });
+      mediaRecorder.current = media;
+      mediaRecorder.current.start();
+      let localAudioChunks: Blob[] = [];
+      mediaRecorder.current.ondataavailable = (event) => {
+        if (typeof event === "undefined") return;
+        if (event.data.size === 0) return;
+        localAudioChunks.push(event.data);
+      };
+      setAudioChunks(localAudioChunks);
+    } else {
+      alert("stream is null");
     }
   };
+  const stopRecording = () => {
+    if (mediaRecorder.current) {
+      setRecordingStatus(RecordingStatus.Inactive);
+      mediaRecorder.current.stop();
+      mediaRecorder.current.onstop = () => {
+        const audioBlob = new Blob(audioChunks, { type: mimeType });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        setAudio(audioUrl);
+        setAudioChunks([]);
+      };
+    } else {
+      alert("mediaRecorder is null");
+    }
+  };
+
+  const submitOrder = () => {
+    if (audio) {
+      let fd = new FormData();
+      fd.append("audio", audio);
+
+      // TODO: Send POST Request
+      alert("Send POST Request (not implemented yet)");
+      return axios.post<OrderResponse>(API_URL + "/order", fd).then((res) => {
+        return res;
+      });
+    }
+  };
+
+  // TODO: Rule based order interpretation
+  const interpretOrder = () => {};
 
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div className="mt-20 font-bold text-5xl">Noodle Shop</div>
       <div className="m-5 text-2xl">Order using Your Voice!</div>
-      <div
-        className={
-          "flex select-none p-3 w-96 h-20 items-center justify-center text-center  border-2 border-gray-500 " +
-          (permission ? "" : "hover:bg-gray-700 active:bg-gray-500")
-        }
-        onClick={getMicrophonePermission}
-      >
-        <span
-          className={"m-auto text-2xl " + (permission ? "text-red-400" : "")}
+      {!permission && (
+        <button
+          className="flex select-none p-3 w-96 h-20 items-center justify-center text-center border-2 border-gray-500 hover:bg-gray-700 active:bg-gray-500 text-2xl"
+          onClick={getMicrophonePermission}
         >
-          {permission ? "Recording" : "Allow Microphone Permission"}
-        </span>
-      </div>
+          Allow Microphone Permission
+        </button>
+      )}
+      {permission && (
+        <div id="audio-controls">
+          {recordingStatus == RecordingStatus.Inactive && (
+            <button
+              className="flex select-none p-3 w-96 h-20 items-center justify-center text-center border-2 border-gray-500 hover:bg-gray-700 active:bg-gray-500 text-2xl"
+              onClick={startRecording}
+            >
+              Start Recording
+            </button>
+          )}
+          {recordingStatus == RecordingStatus.Recording && (
+            <button
+              className="flex select-none p-3 w-96 h-20 items-center justify-center text-center border-2 border-gray-500 hover:bg-gray-700 active:bg-gray-500 text-2xl text-red-500"
+              onClick={stopRecording}
+            >
+              Stop Recording
+            </button>
+          )}
+        </div>
+      )}
+      {audio && (
+        <div className="flex flex-col m-20">
+          <div className="text-2xl">Your Order</div>
+          <button
+            className="flex select-none p-3 w-96 h-20 items-center justify-center text-center border-2 border-gray-500 hover:bg-gray-700 active:bg-gray-500 text-2xl"
+            onClick={submitOrder}
+          >
+            Submit Order
+          </button>
+
+          {/* TODO: Transcription Return from API */}
+          <div>Place Holder: Order Return From API</div>
+        </div>
+      )}
+      {audio && (
+        <div className="flex flex-col m-20 items-center justify-center">
+          <div className="text-2xl">Recording</div>
+          <audio className="mt-5" src={audio} controls></audio>
+          <Link
+            href={audio}
+            className="flex mt-5 select-none p-3 w-64 h-10 items-center justify-center text-center border-2 border-gray-500 hover:bg-gray-700 active:bg-gray-500 text-lg"
+          >
+            Download
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
